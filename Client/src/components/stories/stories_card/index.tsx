f43b393fc3855054147a873f65f17b1c8eb21445
MTalha250 <talhabinay@gmail.com>
2025-02-23 14:50:45 +0500

    update

--- Client/src/components/stories/stories_card/index.tsx
+++ Client/src/components/stories/stories_card/index.tsx
@@ -1,30 +1,24 @@
-import { motion } from "framer-motion"
+import { motion } from "framer-motion";
 interface StoriesCardProps {
-    image : string,
-    title : string,
-    description : string
+  image: string;
+  title: string;
+  description: string;
 }
 
-const StoriesCard = ({image, title, description} : StoriesCardProps) => {
-    return (
-        <motion.div
-        initial={{ opacity: 0, scale:0.7 }}
-        whileInView={{ opacity: 1, scale:1 }}
-        transition={{ duration: 0.9, delay: 0.25 }}
-        viewport={{ once: true }}
-        className="relative object-cover w-[300px] h-[350px] p-5 flex flex-col justify-end rounded-xl"
-            style={{
-                backgroundImage: `url(${image})`,
-                backgroundPosition: 'center',
-            }}
-        >
-            <div className="absolute inset-0 bg-gradient-to-t from-black to-transparent rounded-xl" />
-            <div className="relative z-10">
-                <p className="para-large font-semibold text-white">{title}</p>
-                <p className="para-medium text-white !font-light">{description}</p>
-            </div>
-        </motion.div>
-    )
-}
+const StoriesCard = ({ image, title, description }: StoriesCardProps) => {
+  return (
+    <motion.div
+      initial={{ opacity: 0, scale: 0.7 }}
+      whileInView={{ opacity: 1, scale: 1 }}
+      transition={{ duration: 0.9, delay: 0.25 }}
+      viewport={{ once: true }}
+      className="w-[350px] space-y-2"
+    >
+      <img src={image} alt="card" className="w-full h-[200px]  object-cover" />
+      <p className="font-medium">{title}</p>
+      <p className="font-light">{description}</p>
+    </motion.div>
+  );
+};
 
 export default StoriesCard;
